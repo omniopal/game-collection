2b2e356994eb94f8383885665e109b9f1f2ccd4c
omniopal <jake_123_abc@hotmail.com>
2024-08-06 15:06:53 -0600

    gh-pages troubleshooting: try this again

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Outputs a Single-Page Application (SPA).
+    // output: 'export', // Outputs a Single-Page Application (SPA).
     distDir: './dist', // Changes the build output directory to `./dist/`.
     // TODO
-    images: {
-      unoptimized: true,
-    }
+    // images: {
+    //   unoptimized: true,
+    // }
   }
    
   export default nextConfig
