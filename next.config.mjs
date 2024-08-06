4a33d4d59777d03a7cd12298b11a45476cb3045a
omniopal <jake_123_abc@hotmail.com>
2024-08-06 14:17:39 -0600

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export', // Outputs a Single-Page Application (SPA).
+    output: 'export', // Outputs a Single-Page Application (SPA).
     distDir: './dist', // Changes the build output directory to `./dist/`.
+    basePath: '/game-collection',
+    assetPrefix: '/game-collection',
   }
    
   export default nextConfig
