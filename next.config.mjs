efbe22825fe3a0cec6ecfe716300662a7456e542
omniopal <jake_123_abc@hotmail.com>
2024-08-06 15:31:28 -0600

    refactor: change build output directory back to dist

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Outputs a Single-Page Application (SPA).
+    distDir: './dist', // Changes the build output directory to `./dist/`.
     images: {
-      // TODO
+    // TODO
       unoptimized: true,
     },
     basePath: '/game-collection',
