e22a2e644fc3916b00b12d931590f3a9336f82f7
omniopal <jake_123_abc@hotmail.com>
2024-08-06 20:08:27 -0600

    build: try this with new workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Outputs a Single-Page Application (SPA).
-    distDir: './out', // Changes the build output directory to `./dist/`.
+    distDir: './dist', // Changes the build output directory to `./dist/`.
     images: {
     // TODO
       unoptimized: true,
