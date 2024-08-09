b1180b3ddb515ad52a3b4f771157943047c9cd7e
omniopal <jake_123_abc@hotmail.com>
2024-08-09 12:50:56 -0600

    feat: add new mazes route, check to see if this works on mobile

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { GameCollectionList } from './components/game-collection-list';
-
-function App() {
-  return <GameCollectionList />
-}
-
-export default App;
---
+++ src/app/mazes/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import './mazes.css';
+
+export default function Page() {
+    return <div className="mazes">Mazes</div>
+  }
