e6b2124f24e569d2e643ee9739a4cf62bff04e55
omniopal <jake_123_abc@hotmail.com>
2025-01-26 15:14:42 -0700

    refactor homepage/NES

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -1,43 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import './homepage.css';
-import clsx from "clsx";
-import Typewriter from 'typewriter-effect';
-import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import { NavBar } from "../navbar/navbar";
 import { Reveal } from "../reveal/reveal";
+import { NES } from "../nes/NES";
 
 type HomepageProps = {};
 
 export const Homepage: React.FC<HomepageProps> = (props) => {
     const {} = props;
-    const [poweredOn, setPoweredOn] = useState(false);
-    const [resetButtonClicked, setResetButtonClicked] = useState(false);
-    const [resetButtonKey, setResetButtonKey] = useState(0);
-
-    const handlePowerButtonMouseDown = () => {
-        setPoweredOn(!poweredOn);
-    }
-
-    const handleResetButtonDown = () => {
-        setResetButtonKey(resetButtonKey + 1);
-        setResetButtonClicked(true);
-    }
-
-    const handleResetButtonUp = () => {
-        setResetButtonClicked(false);
-    }
-
-    const arrowButtonStyle = {
-        width: "30px",
-        background: 'none',
-        border: 0,
-    };    
-
-    const slideProperties = {
-        prevArrow: <button style={{ ...arrowButtonStyle }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"><path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z"/></svg></button>,
-        nextArrow: <button style={{ ...arrowButtonStyle }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"><path d="M512 256L270 42.6v138.2H0v150.6h270v138z"/></svg></button>
-    }
 
     useEffect(() => {
         const sliders = document.querySelectorAll('.slide-in');
@@ -66,129 +37,7 @@ export const Homepage: React.FC<HomepageProps> = (props) => {
     return (
         <>
             <NavBar />
-            {/* NES */}
-            <div className="hero">
-                <div className="nes-top">
-                    <div className="nes-top-left">
-                        <div className="cart-slot-outline">
-                            <div className="cart-slot-text name">
-                                I am Jacob Mauro
-                            </div>
-                            <div className="cart-slot-text title">
-                                <Typewriter
-                                    key={resetButtonKey}
-                                    options={{
-                                        wrapperClassName: 'typewriter',
-                                        cursorClassName: 'typewriter-cursor',
-                                        strings: ['Software Engineer', 'UI/UX Enthusiast ', 'Video Game Collector', 'Husband'],
-                                        autoStart: true,
-                                        loop: true,
-                                        cursor: '_',
-                                    }}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="nes-top-middle"></div>
-                    <div className="nes-top-right"></div>
-                </div>
-                <div className="nes-middle" />
-                <div className="nes-bottom">
-                    <div className="nes-bottom-left">
-                        <div className="buttons">
-                            <div className={clsx("power-light", poweredOn ? "powered-on" : "powered-off")} />
-                            <div 
-                                className={clsx("power-button", "nes-buttons", poweredOn && "nes-buttons-pushed")}
-                                onMouseDown={handlePowerButtonMouseDown}
-                            >
-                                <div className="nes-buttons-text">Power</div>
-                            </div>
-                            <div 
-                                className={clsx("reset-button", "nes-buttons", resetButtonClicked && "nes-buttons-pushed")}
-                                onMouseDown={handleResetButtonDown}
-                                onTouchStart={handleResetButtonDown}
-                                onMouseUp={handleResetButtonUp}
-                                onTouchEnd={handleResetButtonUp}
-                            >
-                                <div className="nes-buttons-text">Reset</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="nes-bottom-middle">
-                        <div className="controller-ports">
-                            <div className="controller-port-container">
-                                <div className="controller-port">
-                                    <div className="connector-row">
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                        <div className="controller-port-connector hidden-connector" />
-                                    </div>
-                                    <div className="connector-row">
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                    </div>
-                                    <div className="connector-row">
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                    </div>
-                                    <div className="connector-row">
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="controller-port-container second-controller-port">
-                                <div className="controller-port">
-                                    <div className="connector-row">
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                        <div className="controller-port-connector hidden-connector" />
-                                    </div>
-                                    <div className="connector-row">
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                    </div>
-                                    <div className="connector-row">
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                    </div>
-                                    <div className="connector-row">
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                        <div className="controller-port-connector">
-                                            <div className="pin" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="nes-bottom-right"></div>
-                </div>
-            </div>
+            <NES />
             {/* About me */}
             <div className="wave">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
